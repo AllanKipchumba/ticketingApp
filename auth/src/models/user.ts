26ce66da1c0b7f41798b0dffc949e657a8ba26d7
Allan Kipchumba <allankipchumba76@gmail.com>
2023-07-11 06:38:56 +0300

    formatted user data that is to be sent back

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -19,16 +19,29 @@ interface UserDoc extends mongoose.Document {
   createdAt: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  //sanitize user data to be returned
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 //hash user password
 userSchema.pre("save", async function (done) {
