f68fcf4496d048ac3f1722e35928ce2fc3b0b493
Allan Kipchumba <70691899+AllanKipchumba@users.noreply.github.com>
2023-07-16 12:11:38 +0300

    establishing base url for communicating with gateway from browser and server -wip

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,32 @@
 import axios from "axios";
 
 const LandingPage = ({ currentUser }) => {
-  //console.log(currentUser);
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  if (window === "undefined") {
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers);
+  if (typeof window === "undefined") {
     //we are on the server
-    //requests should be made to 'http://ingress-ingnx-ingress-nginx'
+    //requests should be made to 'http://ingress-ingnx-...'
+    //cross namespace service communication
+    try {
+      const { data } = await axios.get(
+        "http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser",
+        {
+          headers: req.headers,
+        }
+      );
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   } else {
     //we are on the browser
-    //requests can be made with a base url of '
+    //requests can be made with a base url of ''
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
   }
   return {};
 };
