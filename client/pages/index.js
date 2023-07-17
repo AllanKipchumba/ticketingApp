f85d510118a44258e9933e223a4a154316487f2f
Allan Kipchumba <70691899+AllanKipchumba@users.noreply.github.com>
2023-07-17 07:50:00 +0300

    completd sign-up sign-in routes

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,34 +1,17 @@
-import axios from "axios";
+import buildClient from "../api/buildClient";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  console.log(req.headers);
-  if (typeof window === "undefined") {
-    //we are on the server
-    //requests should be made to 'http://ingress-ingnx-...'
-    //cross namespace service communication
-    try {
-      const { data } = await axios.get(
-        "http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser",
-        {
-          headers: req.headers,
-        }
-      );
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    //we are on the browser
-    //requests can be made with a base url of ''
-    const { data } = await axios.get("/api/users/currentuser");
-    return data;
-  }
-  return {};
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
 };
 
 export default LandingPage;
