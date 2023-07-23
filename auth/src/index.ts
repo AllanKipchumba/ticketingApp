6c5cc1dc5a1d25e2caf86f425971110aa8021107
allan <allankipchumba76@gmail.com>
2023-07-23 12:39:58 +0300

    written deployment files to configure tickets and tickets mongo database. changed mongoose connection string to an env var

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,8 +6,12 @@ const start = async () => {
     throw new Error("JWT_KEY must be defined");
   }
 
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("connected to mongoDB");
   } catch (error) {
     console.error(error);
