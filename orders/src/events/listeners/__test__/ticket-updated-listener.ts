2a302765518242e82a13481aa08a07c9549919ab
allan <allankipchumba76@gmail.com>
2023-08-03 07:19:22 +0300

    testing events in orders service -wip

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.ts
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+import {
+  TicketCreatedEvent,
+  TicketUpdatedEvent,
+} from "@ak-tickets-reuse/common";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models/ticket";
+import { TicketUpdateListener } from "../ticket-updated-listener";
+
+const setup = async () => {
+  //create a listener
+  const listener = new TicketUpdateListener(natsWrapper.client);
+
+  //create and save a ticket
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  //create a fake data object
+  const data: TicketUpdatedEvent["data"] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: "new cocert",
+    price: 999,
+    userID: "asdfghjkl",
+  };
+
+  //create a fake msg object
+  //@ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  //return all of the above
+  return { msg, data, ticket, listener };
+};
+
+it("finds, updates and save a ticket", async () => {
+  const { msg, data, ticket, listener } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.title).toEqual(data.title);
+  expect(updatedTicket!.price).toEqual(data.price);
+  expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it("acks the message", async () => {
+  const { msg, data, ticket, listener } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
+
+it("does not call ack if the event has a skipped version number", async () => {
+  const { msg, data, ticket, listener } = await setup();
+
+  data.version = 10;
+
+  try {
+    await listener.onMessage(data, msg);
+  } catch (error) {}
+
+  expect(msg.ack).not.toHaveBeenCalled();
+});
